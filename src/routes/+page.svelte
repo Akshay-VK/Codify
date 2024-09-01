7d06d57f9db2e813342cfd97253a3a8e70e1e9eb
Akshay VK <akshaykaruvathil@gmail.com>
2024-09-01 14:45:20 +0530

    Starting config file loading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,18 @@
+<script>
+  import { resolveResource } from '@tauri-apps/api/path'
+  // alternatively, use `window.__TAURI__.path.resolveResource`
+  import { readTextFile } from '@tauri-apps/api/fs'
+  // alternatively, use `window.__TAURI__.fs.readTextFile`
+
+  async function getConfigData(){
+    // `lang/de.json` is the value specified on `tauri.conf.json > tauri > bundle > resources`
+    const resourcePath = await resolveResource('lang/de.json')
+    const langDe = JSON.parse(await readTextFile(resourcePath))
+
+    console.log(langDe.hello) // This will print 'Guten Tag!' to the devtools console
+  }
+</script>
+
 <h1 class="text-3xl font-bold underline">
   Hello world!
 </h1>
