81b3b972e38a942f6b9372c9cae79a4899957ddf
Akshay VK <akshaykaruvathil@gmail.com>
2024-09-08 23:52:15 +0530

    Added base config YAML code and corresponding Rust structs. Also a bit of re-organising

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use serde::{Serialize, Deserialize};
+mod config;
 
 fn main() {
   tauri::Builder::default()
@@ -11,25 +11,14 @@ fn main() {
   
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Config{
-  actions: Vec<Action>
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Action{
-  name: String,
-  commands: Vec<String>
-}
-
 #[tauri::command]
-fn file_test(handle: tauri::AppHandle) -> String {
+fn file_test(handle: tauri::AppHandle) -> Vec<String> {
   let resource_path = handle.path_resolver()
      .resolve_resource("resources/config.default.yaml")
      .expect("failed to resolve resource");
 
    let file = std::fs::File::open(&resource_path).unwrap();
-   let conf: Config = serde_yml::from_reader(file).unwrap();
+   let conf: config::Config = serde_yml::from_reader(file).unwrap();
 
-   conf.actions[1].name.clone()
+   conf.actions[0].commands.clone()
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,23 @@
   import { Button, FAB } from "m3-svelte";
   import iconEdit from "@ktibow/iconset-material-symbols/edit-outline";
 
-  let output='';
+  let output=[];
 
 
   async function getConfigData(){
     output=await invoke('file_test')
+    console.log(output);
   }
 </script>
 
 <h1 class="text-3xl font-bold underline">
   Hello world!
-  <Button type="filled" on:click="{getConfigData}">Hello world! The name of first entry is {output}</Button>
+  <Button type="filled" on:click="{getConfigData}">Hello world!</Button>
+  <br>
   <FAB color='primary' size='normal' icon={iconEdit} text='Hello' />
+  <p>
+    {#each output as cmd}
+      <i>{cmd}</i>
+    {/each}
+  </p>
 </h1>
