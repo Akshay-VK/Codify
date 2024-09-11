70bcc2cf8ac4ab0381696b5a43d97ca2134c3510
Akshay VK <akshaykaruvathil@gmail.com>
2024-09-11 19:34:27 +0530

    Forgot to include config.rs in commit

---
+++ src-tauri/src/config.rs
@@ -0,0 +1,30 @@
+use serde::{Serialize, Deserialize};
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Config{
+    pub baseLocation: String,
+    pub folders: Vec<Folder>,
+    pub actions:Vec<Action>,
+    pub projectTemplates:Vec<Template>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Folder{
+    pub name: String,
+    pub folder: String
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Action{
+    pub name: String,
+    pub arguments: Vec<String>,
+    pub commands: Vec<String>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Template{
+    pub name: String,
+    pub arguments: Vec<String>,
+    pub commands: Vec<String>
+}
